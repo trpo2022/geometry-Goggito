66f286230c37a239ffcc2ae193cc73446ffb895e
Maxim Yatcuk <Goggito@users.noreply.github.com>
2022-02-14 11:20:45 +0700

    Add file reading in main()

--- main.cpp
+++ main.cpp
@@ -4,8 +4,19 @@
 #include <fstream>
 #include <string>
 using namespace std;
-int main
+int main()
 {
- 
-return 0;
+	std::string line;
+	
+	std::ifstream in("wkt.wkt");
+	if(in.is_open())
+	{
+		while(getline(in, line))
+		{
+			std::cout << line << std::endl;
+		}
+	}
+	in.close();
+	std::cout << "End of programm" << std::endl; 
+	return 0;
 }
