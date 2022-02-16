cdbf61200e3ba88287d8bc34439da1e60489fd9a
Maxim Yatcuk <Goggito@users.noreply.github.com>
2022-02-16 18:32:32 +0700

    Writing read lines to an array

--- main.cpp
+++ main.cpp
@@ -4,18 +4,26 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
 int main()
 {
-	std::string line;
 	
+	int counter = 0;
+	int storage_size = 500;
+	string storage[storage_size];
+	
+	std::string line;
 	std::ifstream in("wkt.wkt");
-	if(in.is_open())
+	
+	if (in.is_open())
 	{
 		while(getline(in, line))
 		{
-			std::cout << line << std::endl;
+			storage[counter] = line;
+			counter++;
 		}
 	}
+	
 	in.close();
 	std::cout << "End of programm" << std::endl; 
 	return 0;
